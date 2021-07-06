6f35c89502f441d3b88ee7c436391338073a4ded
SimonLi <likailong@huawei.com>
2021-07-06 15:23:07 +0800

    fix(build): 修正HAVE_PRODUCT_CONFIG的判断
    hdf_driver.mk和hdf_lite.mk文件中对PRODUCT_PATH只判断了产品的config目录是否存在
    ，而没有判断config目录下的hdf.hcs配置文件是否存在，导致存在PRODUCT_PATH目录下
    存在，没有hdf.hcs文件的情况下，HAVE_PRODUCT_CONFIG宏为y，最终导致编译失败。
    
    fix #I3ZBWA
    
    Signed-off-by: SimonLi <likailong@huawei.com>

--- khdf/liteos/hdf_lite.mk
+++ khdf/liteos/hdf_lite.mk
@@ -80,7 +80,7 @@ endif
 SOURCE_ROOT := $(abspath $(LITEOSTOPDIR)/../../)
 PRODUCT_CONFIG := $(PRODUCT_PATH)/config
 DEVICE_CONFIG := $(SOURCE_ROOT)/$(dir $(subst ",,$(LOSCFG_BOARD_CONFIG_PATH)))
-HAVE_PRODUCT_CONFIG := $(shell if [ -d $(PRODUCT_CONFIG) ]; then echo y; else echo n; fi)
+HAVE_PRODUCT_CONFIG := $(shell if [ -e $(PRODUCT_CONFIG)/Makefile ]; then echo y; else echo n; fi)
 
 ifeq ($(LOSCFG_DRIVERS_HDF_TEST), y)
     # for test
