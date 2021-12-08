14226c081d0f42fa6f87a419736eee54fc3ea40a
weidongwang@bestechnic.com <weidongwang@bestechnic.com>
2021-12-08 21:01:11 +0800

    feat: support drivers adapter platform
    Signed-off-by: weidongwang@bestechnic.com <weidongwang@bestechnic.com>
    Change-Id: I64a643f5df2e4ccf6224337bb3ea8d4ec2e4a5cc

---
+++ platform/i2c/i2c_bes.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2021 Bestechnic (Shanghai) Co., Ltd. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _I2C_BES_H__
+#define _I2C_BES_H__
+
+#include "hal_i2c.h"
+#include "hal_iomux.h"
+#include "hal_iomux_best2003.h"
+#include "device_resource_if.h"
+#include "osal_mutex.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct I2cResource {
+    uint32_t port;
+    uint32_t mode;
+    uint32_t sclPin;
+    uint32_t sdaPin;
+    uint32_t useSync;
+    uint32_t useDma;
+    uint32_t asMaster;
+    uint32_t speed;
+    uint32_t addrAsSlave;
+    uint32_t addressWidth;
+};
+
+struct I2cDevice {
+    uint16_t devAddr;      /**< slave device addr */
+    uint32_t addressWidth; /**< Addressing mode: 7 bit or 10 bit */
+    struct OsalMutex mutex;
+    uint32_t port;
+    struct HAL_I2C_CONFIG_T i2cCfg;
+    struct I2cResource resource;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
